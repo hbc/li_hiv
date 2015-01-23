7edcfde19879de0d00146823c2b88c78d5e4b54f
roryk <rory.kirchner@gmail.com>
2015-01-23 17:03:31 -0500

    Convert the output from the ucsc table browser to BED format.
    Splits the transcript ids, converts to symbols and extracts
    the intron/exon/utr/etc features.

---
+++ scripts/ucsc-bed-converter.R
@@ -0,0 +1,37 @@
+x = "/Users/rory/cache/li_hiv/metadata/hg19_gene_features.bed"
+bed = read.table(x, header=FALSE, sep="\t", stringsAsFactors=FALSE)
+
+get_transcript_id = function(id) {
+    transcript_id = strsplit(id, "_")[[1]][1]
+    return(transcript_id)
+}
+    
+get_feature = function(id) {
+    feature = strsplit(id, "_")[[1]][2]
+    return(feature)
+}
+
+bed$feature = unlist(lapply(bed$V4, get_feature))
+bed$transcript_id = unlist(lapply(bed$V4, get_transcript_id))
+
+bed = bed[, c("V1", "V2", "V3", "transcript_id", "feature")]
+
+library(biomaRt)
+human = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+conversions = getBM(attributes=c("ensembl_transcript_id", "hgnc_symbol"), mart=human)
+
+library(dplyr)
+
+collapse = function(x) {
+    l = list(x)
+    return(paste(l[[1]], collapse=","))
+}
+collapsed = conversions %>% group_by(ensembl_transcript_id) %>%
+    summarise(conv=collapse(hgnc_symbol))
+rownames(collapsed) = collapsed$ensembl_transcript_id
+
+m = merge(bed, collapsed, by.x="transcript_id", by.y="ensembl_transcript_id", all.x=TRUE)
+bed$symbol = collapsed[bed$transcript_id,]$conv
+
+m = m[, c("V1", "V2", "V3", "conv", "feature")]
+write.table(m, file="features.bed", col.names=FALSE, sep="\t", quote=FALSE, row.names=FALSE)
