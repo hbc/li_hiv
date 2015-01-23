83c4d648354871d59d202815d885f73cb65d76a5
roryk <rory.kirchner@gmail.com>
2015-01-23 14:15:07 -0500

    Dump out chimeric reads mapping to HIV to a BAM file.
    Function to look up overlap in the HIV regions bed file for
    chimeric reads.

--- scripts/insertion.py
+++ scripts/insertion.py
@@ -3,8 +3,9 @@ pull out insertion sites from an alignment file aligned with bwa mem
 BWA-mem sets the supplementary alignment flag (2048) and sets a SA tag for
 reads with supplementary alignments
 """
-
+import os
 import pysam
+import pybedtools as bedtools
 import re
 from argparse import ArgumentParser
 
@@ -28,7 +29,9 @@ def get_SA_items(read):
     strand = items[2]
     cigar = items[3]
     bases = [int(x) for x in re.compile("[A-Z]").split(cigar) if x]
-    return chrom, pos, int(pos) + sum(bases), strand
+    sa_mapq = items[4].split(";")[0]
+    nm = items[5].split(";")[0]
+    return chrom, pos, int(pos) + sum(bases), strand, sa_mapq, nm
 
 def get_SA_tag(read):
     SA = [x for x in read.tags if x[0] == 'SA']
@@ -53,14 +56,17 @@ def is_chimera(read, contig, contig_name):
         return False
 
 def parse_cigar_tuples(tuples):
-    d = {"insertions": 0,
+    d = {"clipped_front": 0,
+         "insertions": 0,
          "deletions": 0,
          "clipped": 0,
          "matched": 0,
          "other": 0}
-    for t in tuples:
+    for i, t in enumerate(tuples):
         if t[0] == 4 or t[0] == 5:
             d["clipped"] += t[1]
+            if i == 0:
+                d['clipped_front'] += t[1]
         elif t[0] == 1:
             d["insertions"] += t[1]
         elif t[0] == 2:
@@ -71,51 +77,80 @@ def parse_cigar_tuples(tuples):
             d["other"] += t[1]
     return d
 
+def skip_read(read, contig, virus_contig, in_handle):
+    skip = False
+    if read.is_secondary:
+        skip = True
+    elif is_supplementary(read):
+        skip = True
+    elif not has_supplementary(read):
+        skip = True
+    elif not is_chimera(read, contig, virus_contig):
+        skip=True
+    if read.is_duplicate:
+        skip=True
+    return skip
+
+def make_hiv_feature_function(bed_file):
+    """
+    Create a function that takes a chromosome and position and returns all
+    overlapping subfeatures from the HIV BED file.
+
+    The BED file must be formatted like this:
+    subtype_b       1       634     5-ltr
+
+    calling the returned function with foo("subtype_b", 200) will return 5-ltr
+    """
+    bed = bedtools.BedTool(bed_file)
+    def hiv_overlap(chrom, pos):
+        region = bedtools.BedTool("%s %s %s" % (chrom, pos, pos), from_string=True)
+        overlap = region.intersect(bed, wao=True)
+        features = [x.fields[6] for x in overlap if x.fields[6] != "."]
+        return ",".join(features)
+    return hiv_overlap
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--virus-bed", help="BED file of viral features.")
     parser.add_argument("bamfile", help="BAM file to call insertion sites from")
     parser.add_argument("virus_contig", help="Name of virus contig")
     args = parser.parse_args()
 
     HEADER = "\t".join(["rid", "first", "chrom", "pos", "end", "strand", "mapq",
                         "SA_chrom", "SA_pos", "SA_end", "SA_strand",
+                        "SA_mapq", "SA_nm",
                         "as", "xs", "clipped", "insertions", "deletions",
-                        "matched", "other"])
+                        "matched", "clipped_front"])
     FORMAT = ("{rid}\t{first}\t{chrom}\t{pos}\t{end}\t{strand}\t{mapq}\t{SA_chrom}\t"
-              "{SA_pos}\t{SA_end}\t{SA_strand}\t{AS}\t{XS}\t{clipped}\t"
-              "{insertions}\t{deletions}\t{matched}\t{other}")
+              "{SA_pos}\t{SA_end}\t{SA_strand}\t{SA_mapq}\t{SA_nm}\t{AS}\t{XS}\t{clipped}\t"
+              "{insertions}\t{deletions}\t{matched}\t{clipped_front}")
 
-    with pysam.Samfile(args.bamfile, "rb") as in_handle:
+    chimeric_fn = os.path.splitext(args.bamfile)[0] + ".chimeric.bam"
+
+    with pysam.Samfile(args.bamfile, "rb") as in_handle, \
+         pysam.Samfile(chimeric_fn, "wb", template=in_handle) as out_handle:
         contig = in_handle.gettid(args.virus_contig)
         print HEADER
         for read in in_handle:
-            if read.is_secondary:
+            if skip_read(read, contig, args.virus_contig, in_handle):
                 continue
-            if is_supplementary(read):
-                continue
-            if not has_supplementary(read):
-                continue
-            if not is_chimera(read, contig, args.virus_contig):
-                continue
-            if read.is_duplicate:
-                continue
-            rid = read.qname
-            first = read.is_read1
             chrom = in_handle.getrname(read.tid)
             if chrom == args.virus_contig:
-                pass
+                continue
+            out_handle.write(read)
+            rid = read.qname
+            first = read.is_read1
             strand = "-" if read.is_reverse else "+"
             pos = read.pos
             end = read.pos + read.reference_length
             AS = get_tag(read, "AS")
             XS = get_tag(read, "XS")
             mapq = read.mapq
-            SA_chrom, SA_pos, SA_end, SA_strand = get_SA_items(read)
+            SA_chrom, SA_pos, SA_end, SA_strand, SA_mapq, SA_nm = get_SA_items(read)
             cigar = parse_cigar_tuples(read.cigar)
             clipped = cigar["clipped"]
             insertions = cigar["insertions"]
             deletions = cigar["deletions"]
             matched = cigar["matched"]
-            other = cigar["other"]
+            clipped_front = cigar["clipped_front"]
             print FORMAT.format(**locals())
