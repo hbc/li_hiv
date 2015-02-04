770a9b4c00537710fad75253109c6170e76fb775
roryk <rory.kirchner@gmail.com>
2015-02-04 11:33:29 -0500

    Keep ensembl gene id, since not everything can be resolved to a symbol.

--- scripts/ucsc-bed-converter.R
+++ scripts/ucsc-bed-converter.R
@@ -14,11 +14,12 @@ get_feature = function(id) {
 bed$feature = unlist(lapply(bed$V4, get_feature))
 bed$transcript_id = unlist(lapply(bed$V4, get_transcript_id))
 
-bed = bed[, c("V1", "V2", "V3", "transcript_id", "feature")]
+bed = bed[, c("V1", "V2", "V3", "V6", "transcript_id", "feature")]
 
 library(biomaRt)
 human = useMart("ensembl", dataset="hsapiens_gene_ensembl")
-conversions = getBM(attributes=c("ensembl_transcript_id", "hgnc_symbol"), mart=human)
+conversions = getBM(attributes=c("ensembl_gene_id", "ensembl_transcript_id",
+                        "hgnc_symbol"), mart=human)
 
 library(dplyr)
 
@@ -27,11 +28,11 @@ collapse = function(x) {
     return(paste(l[[1]], collapse=","))
 }
 collapsed = conversions %>% group_by(ensembl_transcript_id) %>%
-    summarise(conv=collapse(hgnc_symbol))
+    summarise(conv=collapse(hgnc_symbol), id=collapse(ensembl_gene_id))
 rownames(collapsed) = collapsed$ensembl_transcript_id
 
 m = merge(bed, collapsed, by.x="transcript_id", by.y="ensembl_transcript_id", all.x=TRUE)
-bed$symbol = collapsed[bed$transcript_id,]$conv
+#bed$symbol = collapsed[bed$transcript_id,]$conv
 
-m = m[, c("V1", "V2", "V3", "conv", "feature")]
+m = m[, c("V1", "V2", "V3", "conv", "feature", "id", "V6")]
 write.table(m, file="features.bed", col.names=FALSE, sep="\t", quote=FALSE, row.names=FALSE)
