f66ddea58952119a278f45173dc280a600f5053c
roryk <rory.kirchner@gmail.com>
2015-12-07 16:49:33 -0500

    Instead of dropping reads with two alternates, pick the one mapping better.

--- scripts/check-virusinfo.R
+++ scripts/check-virusinfo.R
@@ -7,10 +7,12 @@ out_fn = args[2]
 t = read_delim(in_fn, delim=" ")
 
 x = t %>% filter(mapq > 20) %>%
-    group_by(read_name, chrom, pos, orientation, insertion_end, seqcode, virus_pos, virus_end) %>% distinct() %>%
-    group_by(read_name) %>% mutate(read_count=n()) %>% filter(read_count == 1) %>%
-    group_by(chrom, pos, orientation, insertion_end, seqcode, virus_pos, virus_end) %>% summarise(count=n())
-x = x[, c("chrom", "pos", "pos", "orientation", "insertion_end", "seqcode", "count", "virus_pos", "virus_end")]
+  group_by(read_name, chrom, pos, orientation, insertion_end, seqcode, virus_pos) %>%
+  distinct() %>%
+  group_by(read_name) %>% mutate(read_count=n()) %>% filter(mapq == max(mapq)) %>%
+  group_by(chrom, pos, orientation, insertion_end, seqcode, virus_pos) %>%
+  summarise(count=n())
+x = x[, c("chrom", "pos", "pos", "orientation", "insertion_end", "seqcode", "count", "virus_pos")]
 
 write.table(x, file=out_fn, row.names=FALSE, quote=FALSE, col.names=FALSE,
             sep="\t")
--- scripts/check.R
+++ scripts/check.R
@@ -7,10 +7,11 @@ out_fn = args[2]
 t = read_delim(in_fn, delim=" ")
 
 x = t %>% filter(mapq > 20) %>%
-    group_by(read_name, chrom, pos, orientation, insertion_end, seqcode) %>% distinct() %>%
-    group_by(read_name) %>% mutate(read_count=n()) %>% filter(read_count == 1) %>%
-    group_by(chrom, pos, orientation, insertion_end, seqcode) %>% summarise(count=n())          
+  group_by(read_name, chrom, pos, orientation, insertion_end, seqcode) %>%
+  distinct() %>%
+  group_by(read_name) %>% mutate(read_count=n()) %>% filter(mapq == max(mapq)) %>%
+  group_by(chrom, pos, orientation, insertion_end, seqcode) %>% summarise(count=n())
 x = x[, c("chrom", "pos", "pos", "orientation", "insertion_end", "seqcode", "count")]
-    
+
 write.table(x, file=out_fn, row.names=FALSE, quote=FALSE, col.names=FALSE,
             sep="\t")
